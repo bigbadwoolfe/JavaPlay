b1cb44249a1c3c99e39a2addc891a2921113acbe
Metodi Baniski <baniski@gmail.com>
2016-08-09 22:14:21 -0400

    Adding Q1 from Linked Lists - removing duplicates from unsorted lists. + Adding a bunch of utilities for linked lists.

--- src/ch2/RemoveDuplicates.java
+++ src/ch2/RemoveDuplicates.java
@@ -2,16 +2,76 @@ package ch2;
 
 import util.Question;
 import util.LinkedListNode;
+import java.util.Hashtable;
+import java.util.Random;
 
 public class RemoveDuplicates extends Question {
   
   public RemoveDuplicates() {
     super("2.1", "Remove duplicates from an unordered linked list");
   }
-  
+
   public void run() {
     hello();
-    
+
+    int run = 1;
+
+    while (run <= 10) {
+      System.out.printf("Run: %d\n", run);
+      Random rand = new Random();
+      LinkedListNode test = LinkedListNode.generateRandomList(rand.nextInt(20) + 1, true);
+      LinkedListNode test2 = test.clone();
+
+      test.printYourself();
+      test2.printYourself();
+      removeDuplicates(test);
+      removeDuplicatesNoBuffer(test2);
+      test.printYourself();
+      test2.printYourself();
+
+      System.out.println("------------------");
+
+      run++;
+    }
+
+
     bye();
+  }
+
+  public void removeDuplicates(LinkedListNode n) {
+    Hashtable hashtable = new Hashtable();
+    LinkedListNode prev = null;
+    while (n != null) {
+      if (hashtable.containsKey(n.data)) {
+        // n needs to be removed
+        prev.next = n.next;
+      } else {
+        // n needs to be added to hashtable
+        hashtable.put(n.data, n);
+      }
+      prev = n;
+      n = n.next;
+    }
+  }
+
+  public void removeDuplicatesNoBuffer(LinkedListNode n) {
+    LinkedListNode stepper = n; // goes through list slowly
+    LinkedListNode runner = null; // goes through list for every step
+    LinkedListNode prevRunner = n;
+
+    while (stepper != null) {
+      if (stepper.next != null) {
+        runner = stepper.next;
+        while (runner != null) {
+          if (prevRunner.data == runner.data) {
+            prevRunner.next = runner.next;
+          }
+          prevRunner = runner;
+          runner = runner.next;
+        }
+      }
+      stepper = stepper.next;
+    }
+
   }
 }
--- src/util/LinkedListNode.java
+++ src/util/LinkedListNode.java
@@ -1,8 +1,10 @@
 package util;
 
+import java.util.Random;
+
 public class LinkedListNode {
-  LinkedListNode next = null;
-  int data;
+  public LinkedListNode next = null;
+  public int data;
   
   public LinkedListNode(int d) {
     this.data = d;
@@ -23,11 +25,63 @@ public class LinkedListNode {
     LinkedListNode newHead = new LinkedListNode(d);
     newHead.next = this;
     return newHead;
-  } 
-  
+  }
+
   public void printYourself() {
     LinkedListNode n = this;
-    
+
+    StringBuffer sb = new StringBuffer();
+
+    sb.append(String.format("%3d ->", n.data));
+
+    while (n.next != null) {
+      n = n.next;
+      sb.append(String.format("%3d -> ", n.data));
+    }
+    System.out.println(sb.toString());
   }
-  
+
+  public static LinkedListNode generateRandomList(int numEl, boolean withDuplicates) {
+    if (numEl < 1) {
+      return null;
+    }
+
+    Random rand = new Random();
+
+    LinkedListNode first = null;
+    LinkedListNode current = null;
+    int i = 1;
+    while (i <= numEl) {
+
+      if (i == 1) {
+        first = new LinkedListNode(rand.nextInt(numEl * 10) + 1);
+        current = first;
+      } else {
+        int val = rand.nextInt(numEl * 10) + 1;
+        current.next = new LinkedListNode(val);
+        current = current.next;
+        if (withDuplicates) {
+          if (i % (rand.nextInt(5) + 1) == 0) {
+            current.next = new LinkedListNode(val);
+            current = current.next;
+          }
+        }
+      }
+      i++;
+    }
+    return first;
+  }
+
+  public LinkedListNode clone() {
+    LinkedListNode n = this;
+    LinkedListNode clone = new LinkedListNode(this.data);
+
+    while (n.next != null) {
+      n = n.next;
+      clone.appendToTail(n.data);
+    }
+
+    return clone;
+
+  }
 }
